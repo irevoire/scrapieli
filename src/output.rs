bee31a6f944ac06f71add4d22123d73e02f6a566
Tamo <tamo@meilisearch.com>
2022-07-07 13:11:19 +0200

    add missing file

---
+++ src/output.rs
@@ -0,0 +1,23 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Output {
+    pub hierarchy_radio_lvl0: Option<String>,
+    pub hierarchy_radio_lvl1: Option<String>,
+    pub hierarchy_radio_lvl2: Option<String>,
+    pub hierarchy_radio_lvl3: Option<String>,
+    pub hierarchy_radio_lvl4: Option<String>,
+    pub hierarchy_radio_lvl5: Option<String>,
+    pub hierarchy_lvl0: Option<String>,
+    pub hierarchy_lvl1: Option<String>,
+    pub hierarchy_lvl2: Option<String>,
+    pub hierarchy_lvl3: Option<String>,
+    pub hierarchy_lvl4: Option<String>,
+    pub hierarchy_lvl5: Option<String>,
+    pub hierarchy_lvl6: Option<String>,
+    pub content: String,
+    #[serde(rename = "objectID")]
+    pub object_id: String,
+    pub anchor: Option<String>,
+    pub url: String,
+}
