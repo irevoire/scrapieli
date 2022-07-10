c3edec4f1299c090578dd80f57a4975070e3c293
mlemesle <lemesle.martin@hotmail.fr>
2022-07-10 15:24:26 +0200

    feature/multi-scraping selectors are correctly parsed

--- src/output.rs
+++ src/output.rs
@@ -1,6 +1,6 @@
 use serde::Serialize;
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 pub struct Output {
     pub hierarchy_radio_lvl0: Option<String>,
     pub hierarchy_radio_lvl1: Option<String>,
